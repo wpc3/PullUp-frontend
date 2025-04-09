50f274002052d40f7607bd41cca1edebbf9ac9a9
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-04-09 13:47:47 -0400

    Created Login React file and added code from previous pull up frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./Component/LoginPage/Login";
 
 function App() {
   return (
     <div className="App">
+              <Login/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           I have a call at 10 <code>PullUpApp</code> Hello, Squiggs
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
