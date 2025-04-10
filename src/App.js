62b779b2d900ec1e8092c25e018709012412e385
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-04-10 09:30:54 -0400

    Removed Default React Background to use as Landing page for PullUp Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Component/LoginPage/Login";
 
@@ -6,22 +5,6 @@ function App() {
   return (
     <div className="App">
               <Login/>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          I have a call at 10 <code>PullUpApp</code> Hello, Squiggs
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-         //comment 
-        </a>
-      </header>
     </div>
   );
 }
